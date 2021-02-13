d961055238396f49fa8a89acf82c9596ec8eb36d
Mario Campopiano <mcampopiano92@gmail.com>
2021-02-13 15:38:53 -0600

    extracted desired info from .pkl files

--- python_basics.py
+++ python_basics.py
@@ -1,5 +1,5 @@
 from keys import ameritrade
-import requests, time, re
+import requests, time, re, os
 import pandas as pd
 import pickle as pkl
 from pprint import pprint
@@ -11,7 +11,7 @@ symbols = list(df['Symbol'].values)
 
 start = 0
 end = 500
-
+files = []
 while start < len(symbols):
     tickers = symbols[start:end]
 
@@ -23,10 +23,23 @@ while start < len(symbols):
     data = results.json()
     f_name = time.asctime() + '.pkl'
     f_name = re.sub('[ :]','_',f_name)
+    files.append(f_name)
     with open(f_name, 'wb') as file:
         pkl.dump(data,file)
     start = end
     end += 500
     time.sleep(1)
 
-    
+data = []
+
+for file in files:
+    with open(file,'rb') as f:
+        info = pkl.load(f)
+    tickers = list(info)
+    points = ['symbol','netProfitMarginMRQ','peRatio','pegRatio','high52']
+    for ticker in tickers:
+        tick = []
+        for point in points:
+            tick.append(info[ticker]['fundamental'][point])
+        data.append(tick)
+    os.remove(file)
